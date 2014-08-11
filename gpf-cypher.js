7d4feaa0d83e511fd7e92039d793d1acbfc42b66
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-08-10 23:52:51 -0400

    WIP

--- gpf-cypher.js
+++ gpf-cypher.js
@@ -12,12 +12,12 @@ window.onlad=gpf.loaded(function () {
                 _fileInput: null,
 
                 "[_onBrowse]": [gpf.$HtmlEvent("click", "div.button.file")],
-                _onBrowse: function(event) {
+                _onBrowse: function (/*event*/) {
                     if (null === this._fileInput) {
                         // TODO find a better way to achieve this
                         var div = document.createElement("div");
-                        div.innerHTML = "<input type=\"file\" id=\"sourceFile\" "
-                            + "style=\"display: none;\">";
+                        div.innerHTML = "<input type=\"file\" id=\"sourceFile\""
+                            + " style=\"display: none;\">";
 
                         this._fileInput = this._ui.appendChild(div.firstChild);
                         // TODO see if it can be done automatically
@@ -29,7 +29,9 @@ window.onlad=gpf.loaded(function () {
                 "[_onBrowsed]": [gpf.$HtmlEvent("change", "input[type=file]")],
                 _onBrowsed: function (event) {
                     var file = event.target.files[0];
-                    alert(file.name);
+                    if (undefined !== file) {
+                        alert(file.name);
+                    }
                 },
 
 
@@ -52,9 +54,19 @@ window.onlad=gpf.loaded(function () {
                 }
 
             }
+        }),
+
+        Source = gpf.define("Source", Box, {
+
+        }),
+
+        KeySelector = gpf.define("Source", Box, {
+
         });
 
-    var source = new Box();
+    var source = new Source();
     gpf.html.handle(source, "#source");
+    var keySelector = new KeySelector();
+    gpf.html.handle(keySelector, "#keySelector");
 
 });
