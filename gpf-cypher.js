c0ba138fcc6fdb34de654a4aeb3b4fd157cd7f4c
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-01-14 13:39:39 -0500

    GPF bootstrap

--- gpf-cypher.js
+++ gpf-cypher.js
@@ -1,1 +1,9 @@
-var gpf = require("gpf");
+var gpf;
+try {
+    gpf = require("gpf-js");
+} catch (e) {
+    console.error("require(gpf-js) failed: use npm install gpf-js");
+    console.error(e);
+    process.exit();
+}
+console.log("Using GPF framework version " + gpf.version());
