b0605cbd25b213a711facab1373c1728bee777d8
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-01-15 13:22:46 -0500

    Created main API

--- core.js
+++ core.js
@@ -1,5 +1,39 @@
 (function () { /* Begin of privacy scope */
     "use strict";
 
+    // Extend existing
+    gpf.cypher = {
+
+        /**
+         * Crypt the input stream using keys and push the result data in the
+         * output stream
+         *
+         * @param {gpf.IReadableStream} input
+         * @param {gpf.IWritableStream} output
+         * @param {gpf.IReadableStream[]} keys
+         * @param {gpf.events.Handler} eventsHandler
+         *
+         * @event ready
+         */
+        crypt: function (input, output, keys, eventsHandler) {
+
+        },
+
+        /**
+         * Decrypt the input stream using keys and push the result data in the
+         * output stream
+         *
+         * @param {gpf.IReadableStream} input
+         * @param {gpf.IWritableStream} output
+         * @param {gpf.IReadableStream[]} keys
+         * @param eventsHandler
+         *
+         * @event ready
+         */
+        decrypt: function (input, output, keys, eventsHandler) {
+
+        }
+
+    };
 
 }()); /* End of privacy scope */
