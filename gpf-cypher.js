b29343636033116b953343015d89a1dfc2640131
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-01-15 23:44:29 -0500

    Added options

--- gpf-cypher.js
+++ gpf-cypher.js
@@ -7,3 +7,29 @@ try {
     process.exit();
 }
 console.log("Using GPF framework version " + gpf.version());
+// Parse command line parameters
+try {
+    var parameters = gpf.Parameter.parse([{
+        name: "input",
+        required: true,
+        type: "string"
+    }, {
+        name: "output",
+        type: "string",
+        defaultValue: "*"
+    }, {
+        name: "key",
+        type: "string",
+        prefix: "key",
+        multiple: true
+    }, gpf.Parameter.VERBOSE, gpf.Parameter.HELP], process.argv.slice(2));
+} catch (e) {
+    console.error(e.message);
+    process.exit();
+}
+
+for (var key in parameters) {
+    if (parameters.hasOwnProperty(key)) {
+        console.log(key + "          : ".substr(key.length) + parameters[key]);
+    }
+}
