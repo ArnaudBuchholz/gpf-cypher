833835b9cd2d8bda2e98e59095d4ec58a7bff543
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-01-19 13:42:18 -0500

    Added core

--- gpf-cypher.js
+++ gpf-cypher.js
@@ -7,6 +7,7 @@ try {
     process.exit();
 }
 console.log("Using GPF framework version " + gpf.version());
+require("core.js");
 // Parse command line parameters
 try {
     var parameters = gpf.Parameter.parse([{
